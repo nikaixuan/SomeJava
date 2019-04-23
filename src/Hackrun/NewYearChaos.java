1ad20b9784d9369213c8e7af478ba37b41e0bc3a
nikaixuan <nkx422@gmail.com>
2019-04-23 23:38:14 +1000

    New problems

---
+++ src/Hackrun/NewYearChaos.java
@@ -0,0 +1,59 @@
+package Hackrun;
+
+import java.util.Scanner;
+
+/**
+ * Created by nikaixuan on 23/4/19.
+ */
+public class NewYearChaos {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+        boolean flag = true;
+        int result = 0;
+        for(int i=0;i<q.length;i++){
+            if(q[i]-i-1>2){
+                flag = false;
+                break;
+            }else{
+                for(int j=Math.max(0,q[i]-2);j<i;j++){
+                    if(q[i]<q[j]){
+                        result++;
+                    }
+                }
+            }
+        }
+        if(flag){
+            System.out.println(result);
+        }else{
+            System.out.println("Too chaotic");
+        }
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
+}
--- src/StringTest.java
+++ src/StringTest.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /**
  * Created by nikaixuan on 15/4/19.
@@ -9,19 +10,85 @@ public class StringTest {
     }
 
      public static void main(String[] args){
-         String s = "abcd";
-         char[] arr = s.toCharArray();
-         String s1 = "cd";
-         String tests = "ab" + s1;
-         System.out.println(s==tests);
-         byte bt = (byte)300;
-         System.out.println(bt);
-         Byte a = 100;
-         Byte a1 = 100;
-         System.out.println(a==a1);
-         int[] intarr = new int[(int)Math.sqrt(arr.length)];
-         System.out.println(Math.pow(arr.length,2));
+//         List<String> a = new ArrayList<>();
+//         List<String> b = new ArrayList<>();
+//         a.add("abcd");
+//         b.add("bacd");
+//         a.add("abcdf");
+//         b.add("acfgd");
+         String s = "tyrew";
+         String result = "";
+         List<String> a = new ArrayList<>();
+         for (int i = 0; i < s.length(); ++i) {
+             a.add(s.substring(i));
+         }
 
+         String[] b = a.stream().toArray(String[]::new);
+         Arrays.sort(b);
+         System.out.println(b[b.length-1]);
 
      }
+
+    public static int countHoles(int num) {
+        // Write your code here
+        int result = 0;
+        while(num>0){
+            int curr_digt = num%10;
+            if(curr_digt==(1|2|3|5|7)){
+
+                result+=0;
+                System.out.println(result);
+
+            }else if(curr_digt==(0|4|6|9)){
+                System.out.println(curr_digt);
+                result+=1;
+                System.out.println(result);
+            }else{
+                System.out.println(curr_digt);
+                result+=2;
+                System.out.println(result);
+            }
+            num/=10;
+        }
+        return result;
+
+    }
+
+
+
+    public static List<String> twins(List<String> a, List<String> b) {
+        // Write your code here
+        int len = a.size();
+        List<String> result = new ArrayList<>();
+        for(int i=0;i<len;i++){
+            Set aodd = new HashSet<>();
+            Set bodd = new HashSet<>();
+            Set aeven = new HashSet<>();
+            Set beven = new HashSet<>();
+            if(a.get(i).length()==b.get(i).length()){
+                for (int j = 0; i < a.get(i).length(); j++) {
+                    if (j % 2 == 0) {
+                        aeven.add(a.get(i).charAt(j));
+                        System.out.println(b.get(i));
+                        System.out.println(a.get(i).length());
+                        beven.add(b.get(i).charAt(j));
+                    } else {
+                        aodd.add(a.get(i).charAt(j));
+                        bodd.add(b.get(i).charAt(j));
+                    }
+                }
+                if (aeven.containsAll(beven) && aodd.containsAll(bodd)) {
+                    result.add("YES");
+                } else {
+                    result.add("NO");
+                }
+            }
+            else{
+                result.add("NO");
+            }
+
+        }
+        return result;
+    }
+
 }
